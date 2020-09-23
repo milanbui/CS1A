8dee724a7c803cd74843dcef6593c0e5443e0cae
Milan B <43893040+milanbui@users.noreply.github.com>
2020-09-23 16:23:37 -0700

    Add files via upload

---
+++ L2B-Average/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	double num1, num2, num3, average;
+
+	num1 = 5.0;
+	num2 = 10.0;
+	num3 = -12.6;
+
+	average = (num1+num2+num3)/3;
+
+	cout << "The first number is " << num1 << endl;
+	cout << "The second number is " << num2 << endl;
+	cout << "The third number is " << num3 << endl;
+	cout << "The average is " << average << endl;
+
+	return 0;
+}
+
+
+
